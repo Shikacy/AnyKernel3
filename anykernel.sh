633de04cef04e67aea1ce6003cbe49023933a8b2
Shikacy <58970008+Shikacy@users.noreply.github.com>
2023-09-02 17:38:25 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,45 +1,33 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
+## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=N0Kernel-KSU by ticks
-do.devicecheck=1
+kernel.string=KernelSU by KernelSU Developers
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=salami
-device.name2=
-device.name3=
-device.name4=
-device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
-### AnyKernel install
-# begin attributes
-attributes() {
-set_perm_recursive 0 0 750 750 $ramdisk/*;
-} # end attributes
-
-
-## boot shell variables
-block=/dev/block/bootdevice/by-name/boot;
+# shell variables
+block=/dev/block/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh && attributes;
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
+# install additional module
+. tools/module-install.sh
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
+flash_boot;
+## end install
